9ecbe15ec3b82f388dd4aef2b34442a88aa7f681
Dv1101 <dhruvvaria1101@gmail.com>
2024-02-05 01:14:43 +0530

    Initial Commit

---
+++ EntityFW_CRUD/Controllers/StudentsController.cs
@@ -0,0 +1,48 @@
+﻿using EntityFW_CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFW_CRUD.Controllers
+{
+    public class StudentsController : Controller
+    {
+        StudentContext db = new StudentContext();
+        // GET: Students
+        public ActionResult Index()
+        {
+            var data = db.Students.ToList();
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            if(ModelState.IsValid)
+            { 
+                db.Students.Add(s);
+                int status = db.SaveChanges();
+                if (status > 0)
+                {
+                    ViewBag.InsertMessage = "<script>alert('Data Insert Success !!')</script";
+                    ModelState.Clear();
+                }
+                else
+                {
+                    ViewBag.InsertMessage = "<script>alert('Data Insert Failed !!')</script";
+                }
+            }
+            return View();
+
+        }
+    }
+}
---
+++ EntityFW_CRUD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntityFW_CRUD.MvcApplication" Language="C#" %>
